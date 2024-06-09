d9d357eba33961e6de28935ad54815d9ba833c8c
sky line <32512284+linesky@users.noreply.github.com>
2024-06-09 16:07:08 +0100

    Add files via upload

--- maker.sh
+++ maker.sh
@@ -1,10 +1,18 @@
 printf "\x1bc\x1b[43;37m"
-roots=$(pwd)/roots
+roots=/mnt/isos
 tmps=/tmp/lists.txt
 tmps2=/tmp/lists2.txt
-as hello.asm -o hello.o
-ld  -dynamic-linker /lib/i386-linux-gnu/ld-linux.so.2 /lib/i386-linux-gnu/libc.so.6 /lib/i386-linux-gnu/libc.a hello.o -o hello
+printf "" > $tmps
+dd if=/dev/zero of="/tmp/my.img" bs=1k count=30000
+sudo chmod 777 $tmps
+sudo chmod 777 $tmps2
+sudo chmod 777 "/tmp/my.img"
+sudo mkfs.vfat "/tmp/my.img" 
+sudo chmod 777 "/tmp/my.img"
+as hello.asm -o /tmp/hello.o
+ld  -dynamic-linker /lib/i386-linux-gnu/ld-linux.so.2 /lib/i386-linux-gnu/libc.so.6 /lib/i386-linux-gnu/libc.a /tmp/hello.o -o /tmp/hello
 mkdir -p $roots
+sudo mount "/tmp/my.img" $roots -o loop -t vfat 
 mkdir -p $roots/usr
 mkdir -p $roots/usr/bin
 mkdir -p $roots/bin
@@ -19,13 +27,6 @@ mkdir -p $roots/lib/i386-linux-gnu
 cp /lib/i386-linux-gnu/ld-linux.so.* $roots/lib/i386-linux-gnu/
 cp /lib/i386-linux-gnu/libc.so.* $roots/lib/i386-linux-gnu/
 cp  /lib/i386-linux-gnu/crt*.* $roots/lib/i386-linux-gnu/
-cp  /usr/include/*.* $roots/usr/include
-cp  ./hello.c $roots/usr/bin
-cp  ./compile.sh $roots/bin
-cp  ./compile.sh $roots/usr/bin
-cp  ./hello.asm $roots/bin
-cp  ./hello.asm $roots/usr/bin
-cp  ./hello.c $roots/bin
 cp  /lib/i386-linux-gnu/libc.a $roots/usr/bin
 cp  /lib/i386-linux-gnu/libc.a $roots/bin
 cp  /lib/i386-linux-gnu/libc.a $roots/lib/i386-linux-gnu/
@@ -45,12 +46,15 @@ cp  /usr/bin/ls $roots/usr/bin
 cp  /usr/bin/ls $roots/bin
 cp  /usr/bin/ldd $roots/usr/bin
 cp  /usr/bin/ldd $roots/bin
+mv /tmp/hello $roots/bin
 printf "" > $roots/dev/null
 printf "" > $roots/dev/stdio
 printf "" > $roots/dev/stdout
 printf "" > $roots/dev/stdin
 chmod 777 $roots/bin/*
 chmod 777 $roots/usr/bin/*
+sudo chmod 777 $tmps
+sudo chmod 777 $tmps2
 printf "" > $tmps
 list1=$(ls $roots/usr/bin/*)
 for l1 in $list1
@@ -64,7 +68,7 @@ do
 rt="$roots$l1"
 cp "$l1" "$rt" 
 done < "$tmps2"
-
+sudo umount  $roots
 
 
 
